c44da37688ffb297a08e29dcfe6d59c4ae1ca554
Andres Hernandez Lozano <soyandresdev@gmail.com>
2020-06-28 22:36:08 -0500

    [ADD] 2-cargo: example project with cargo

---
+++ 2-cargo/project/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world! From Cargo");
+}
+// cargo new  : Create Project with cargo
+// cargo build  : Compile the project
+// cargo run  : Compile and run the project
